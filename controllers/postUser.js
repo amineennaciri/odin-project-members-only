f9ccfbae20e670489c19cc1e5237805f2d51d531
amineennaciri <spitlight.enn@gmail.com>
2023-07-04 11:19:42 +0100

    fixed the sign up form and validation to the database, now will be working on the login setup

--- controllers/postUser.js
+++ controllers/postUser.js
@@ -1,23 +1,75 @@
+const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const User = require('./../models/user');
+const validator = require('validator');
 module.exports = {
     getSignUp: (req,res)=>{
         res.render('signUp.ejs')
     },
     postSignUp: async (req, res, next)=>{
-        bcrypt.hash(req.body.password, 10, async   (err, hashedPassword) => {
-          try {
-            const user = new User({
-              firstName: req.body.firstN,
-              lastName: req.body.lastN,
-              email:req.body.email,
-              password: hashedPassword,
-            });
-            const result = await user.save();
-            res.redirect("/");
-          } catch(err) {
-            return next(err);
-          };
-        });
+      const validationErrors = []
+      if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+      if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
+      if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
+      if (validationErrors.length) {
+        req.flash('errors', validationErrors)
+        return res.redirect('../new')
+      }
+      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+      bcrypt.hash(req.body.password, 10, async   (err, hashedPassword) => {
+        try {
+          const existingUser = await User.findOne({
+            $or: [
+              { email: req.body.email },
+              { firstName: req.body.firstN },
+              { lastName: req.body.lastN }
+            ]
+          });
+          if (existingUser) {
+            req.flash('errors', { msg: 'Account with that email address or username already exists.' });
+            return res.redirect('../new');
+          }
+          const user = new User({
+            firstName: req.body.firstN,
+            lastName: req.body.lastN,
+            email:req.body.email,
+            password: hashedPassword,
+          });
+          //await user.save();
+          //await req.logIn(user);
+          const result = await user.save();
+          res.redirect("/");
+        } catch(err) {
+          return next(err);
+        };
+      });
+      /*
+      User.findOne({$or: [
+        {email: req.body.email},
+        {firstName: req.body.firstN},
+        {lastName: req.body.lastN},
+      ]}, (err, existingUser) => {
+        if (err) { return next(err) }
+        if (existingUser) {
+          req.flash('errors', { msg: 'Account with that email address or username already exists.' })
+          return res.redirect('../new')
+        }
+        user.save((err) => {
+          if (err) { return next(err) }
+          req.logIn(user, (err) => {
+            if (err) {
+              return next(err)
+            }
+            res.redirect('/');
+          })
+        })
+      });
+      */
+/*       try {
+
+      } catch (err) {
+        next(err);
+      } */
+      
     },
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
+const flash = require('express-flash');
 const mainRoutes = require('./routes/index');
 const postRoutes = require('./routes/post');
 const connectDB = require("./config/database");
@@ -17,6 +18,7 @@ require('./config/passport')(passport);
 //Connect To Database
 connectDB();
 app.use(passport.initialize());
+// you will have access to the currentUser variable in all of your views, and you won’t have to manually pass it into all of the controllers in which you need it.
 app.use(function(req, res, next) {
     res.locals.currentUser = req.user;
     next();
@@ -27,6 +29,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+// these are the flash alert that works when something isn't working well with our log ins
+app.use(flash());
 
 app.use("/", mainRoutes);
 app.use("/new", postRoutes);
